c48071a0a4dcd02762ecf43490ba8fb073c36d30
gang018 <gang018@mail.ru>
2013-03-06 22:10:39 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VedroidServer::Application.config.secret_token = '85764773f33c0aa7a2a1a56a74beef0ebd0aa3f4feaea4bffc22f4b45a6a2d699f479400728d59beaaa9e843df48d468247a0a3676c854bb848879f62abb7015'
